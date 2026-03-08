75b84a86d241efe07c6f0165bda1138a45e07952
dev-taosif-net <dev.taosif.net@gmail.com>
2026-03-08 12:13:44 +0600

    Refactor HasEnumConversion method signature for improved readability

--- src/BuildingBlocks/Blocks.EntityFrameworkCore/Extensions/BuilderExtensions.cs
+++ src/BuildingBlocks/Blocks.EntityFrameworkCore/Extensions/BuilderExtensions.cs
@@ -7,8 +7,7 @@ public static class BuilderExtensions
     //.HasConversion<string>() is Used to convert an enum property to a string representation in the database.
     //This allows you to store the enum values as strings instead of their underlying integer values,
     //making the data more readable and easier to understand when querying the database directly.
-    public static PropertyBuilder<TEnum> HasEnumConversion<TEnum>(this PropertyBuilder<TEnum> builder,
-        int maxLength = 64)
+    public static PropertyBuilder<TEnum> HasEnumConversion<TEnum>(this PropertyBuilder<TEnum> builder, int maxLength = 64)
         where TEnum : Enum
     {
         return builder
